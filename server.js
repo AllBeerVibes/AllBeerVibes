6d80ad6ad53b07f43d910a49cec4381d48f0d330
MINHO CHOI <minhocalgary@gmail.com>
2020-06-15 10:24:21 -0600

    make <npm start> work

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const connectDB = require('./db/db');
 
+/** MINHO: npm start is not working until we put the actual link for mongoose
+const connectDB = require('./db/db');
 connectDB(); //connect to mongo
+*/ 
 
 app.get('/', (req, res) => {
 	res.send('Welcome to localhost:5000');
