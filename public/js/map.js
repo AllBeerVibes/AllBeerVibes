eb732bde925c866d7511fb7ed1d754d832f84b94
Billyusmar <bilbarkah.usmar@edu.sait.ca>
2020-07-21 03:59:20 -0600

    Added Geolocation, but no error handling, will break map

--- public/js/map.js
+++ public/js/map.js
@@ -7,21 +7,91 @@
   // This example requires the Places library. Include the libraries=places
   // parameter when you first load the API. For example:
   // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-  function initAutocomplete() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-      center: {
-        lat: -33.8688,
-        lng: 151.2195
-      },
-      zoom: 13,
-      mapTypeId: "roadmap"
-    }); // Create the search box and link it to the UI element.
+  let pos;
+  let map;
+  let bounds;
+  let infoWindow;
+  let currentInfoWindow;
+  let service;
+  let infoPane;
 
+  function initAutocomplete() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: pos,
+          zoom: 15,
+          mapTypeId: "roadmap"
+        });
+  
+        var input = document.getElementById("pac-input");
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input); // Bias the SearchBox results towards current map's viewport.
+    
+        map.addListener("bounds_changed", function() {
+          searchBox.setBounds(map.getBounds());
+        });
+        var markers = []; // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+    
+        searchBox.addListener("places_changed", function() {
+          var places = searchBox.getPlaces();
+    
+          if (places.length == 0) {
+            return;
+          } // Clear out the old markers.
+    
+          markers.forEach(function(marker) {
+            marker.setMap(null);
+          });
+          markers = []; // For each place, get the icon, name and location.
+    
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+    
+            var icon = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            }; // Create a marker for each place.
+    
+            markers.push(
+              new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+              })
+            );
+    
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          map.fitBounds(bounds);
+        });
+      });
+      
+    }
+ 
     function getNearbyPlaces(position) {
       let request = {
           location: position,
           rankBy: google.maps.places.RankBy.DISTANCE,
-          keyword: 'liqour_stores'
+          keyword: 'liquor'
           };
 
       service = new google.maps.places.PlacesService(map);
@@ -34,62 +104,11 @@
         }
       }
 
-    var input = document.getElementById("pac-input");
-    var searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input); // Bias the SearchBox results towards current map's viewport.
 
-    map.addListener("bounds_changed", function() {
-      searchBox.setBounds(map.getBounds());
-    });
-    var markers = []; // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
-
-    searchBox.addListener("places_changed", function() {
-      var places = searchBox.getPlaces();
-
-      if (places.length == 0) {
-        return;
-      } // Clear out the old markers.
-
-      markers.forEach(function(marker) {
-        marker.setMap(null);
-      });
-      markers = []; // For each place, get the icon, name and location.
-
-      var bounds = new google.maps.LatLngBounds();
-      places.forEach(function(place) {
-        if (!place.geometry) {
-          console.log("Returned place contains no geometry");
-          return;
-        }
-
-        var icon = {
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25)
-        }; // Create a marker for each place.
-
-        markers.push(
-          new google.maps.Marker({
-            map: map,
-            icon: icon,
-            title: place.name,
-            position: place.geometry.location
-          })
-        );
-
-        if (place.geometry.viewport) {
-          // Only geocodes have viewport.
-          bounds.union(place.geometry.viewport);
-        } else {
-          bounds.extend(place.geometry.location);
-        }
-      });
-      map.fitBounds(bounds);
-    });
   }
 
   exports.initAutocomplete = initAutocomplete;
-})((this.window = this.window || {}));
+})
+((this.window = this.window || {}));
+
+
