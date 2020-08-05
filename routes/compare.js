4fb9466270e3781eeb6badafac7eca48a75046a1
SooeunKim <sooeuk08@gmail.com>
2020-08-06 08:07:25 +0900

    Removed duplicated code

--- routes/compare.js
+++ routes/compare.js
@@ -14,10 +14,12 @@ const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
 //Endpoint = /compare/my-comparison
 router.get('/my-comparison', (req, res) => {
+	let compare;
+
 	if (req.user) {	
 			CompareTest.findOne({ user: req.user }).then((data) => {
 				if (data != null) {
-					let compare = new Compare(data.compare);
+					compare = new Compare(data.compare);
 					
 					return res.render('compare', {
 						products: compare.generateArray(),
@@ -28,11 +30,10 @@ router.get('/my-comparison', (req, res) => {
 						products: null
 					});
 				}
-			});
-		
+			});	
 	} else {
 		if (req.session.compare) {
-			let compare = new Compare(req.session.compare);
+			compare = new Compare(req.session.compare);
 			return res.render('compare', { products: compare.generateArray(), totalQty: compare.totalQty });
 		} else {
 			return res.render('compare', { products: null }); // To check if compare list is empty or not
@@ -55,12 +56,106 @@ router.get('/clear-compare-list', (req, res) => {
 	res.redirect('/compare/my-comparison');
 });
 
+// router.get('/add-to-compare/:bid', (req, res) => {
+// 	let beerId = req.params.bid;
+// 	let compare;
+
+// 	if (req.user) {
+// 		axios
+// 		.get(apiMethods.getBeerByIdURI(CLIENT_ID, CLIENT_SECRET, beerId))
+// 		.then((response) => {
+// 			let {
+// 				beer_name,
+// 				beer_label,
+// 				beer_label_hd,
+// 				beer_abv,
+// 				beer_ibu,
+// 				beer_description,
+// 				beer_style,
+// 				created_at,
+// 				rating_count,
+// 				rating_score,
+// 				contact,
+// 				location
+// 			} = response.data.response.beer; // beer data
+
+// 			let { brewery_name, brewery_label, country_name } = response.data.response.beer.brewery;
+
+// 			let stars = apiMethods.starRatingElement(response.data.response.beer.rating_score);
+
+// 			CompareTest.findOne({ user: req.user }).then((data) => {
+// 				if (data != null) {
+// 					compare = new Compare(data.compare);
+// 					compare.addBeerCompare(response.data.response.beer, beerId);
+
+// 					CompareTest.findOneAndUpdate({ user: req.user }, { $set: { compare: compare } }, function (err) {
+// 						if (err) {
+// 							req.flash('error', err.message);
+// 							return res.redirect('/compare/my-comparison');
+// 						}
+// 					});
+// 				} else {
+// 					compare = new Compare({});
+// 					compare.addBeerCompare(response.data.response.beer, beerId);
+
+// 					let compareTest = new CompareTest({
+// 						user: req.user,
+// 						compare: compare
+// 					});
+
+// 					compareTest.save(function (err) {
+// 						if (err) {
+// 							req.flash('error', err.message);
+// 							return res.redirect('/compare/my-comparison');
+// 						}
+// 					});
+// 				}
+// 			});
+
+// 			res.redirect('back');
+// 		})	
+// 		.catch((error) => console.error(error));
+// 	} else {
+// 		//1) Check if my Compare property exists
+// 		//2) If it does, pass my old Compare
+// 		//3) Otherwise, pass empty object
+// 		compare = new Compare(req.session.compare ? req.session.compare : {});
+
+// 		axios
+// 		.get(apiMethods.getBeerByIdURI(CLIENT_ID, CLIENT_SECRET, beerId))
+// 		.then((response) => {
+// 			let {
+// 				beer_name,
+// 				beer_label,
+// 				beer_label_hd,
+// 				beer_abv,
+// 				beer_ibu,
+// 				beer_description,
+// 				beer_style,
+// 				created_at,
+// 				rating_count,
+// 				rating_score,
+// 				contact,
+// 				location
+// 			} = response.data.response.beer; // beer data
+
+// 			let { brewery_name, brewery_label, country_name } = response.data.response.beer.brewery;
+
+// 			let stars = apiMethods.starRatingElement(response.data.response.beer.rating_score);
+
+// 			compare.addBeerCompare(response.data.response.beer, beerId);
+// 			req.session.compare = compare;
+// 			res.redirect('back');
+// 		})
+// 		.catch((error) => console.error(error));		
+// 	}
+// });
+
 router.get('/add-to-compare/:bid', (req, res) => {
 	let beerId = req.params.bid;
 	let compare;
 
-	if (req.user) {
-		axios
+	axios
 		.get(apiMethods.getBeerByIdURI(CLIENT_ID, CLIENT_SECRET, beerId))
 		.then((response) => {
 			let {
@@ -82,72 +177,44 @@ router.get('/add-to-compare/:bid', (req, res) => {
 
 			let stars = apiMethods.starRatingElement(response.data.response.beer.rating_score);
 
-			CompareTest.findOne({ user: req.user }).then((data) => {
-				if (data != null) {
-					compare = new Compare(data.compare);
-					compare.addBeerCompare(response.data.response.beer, beerId);
+			if (req.user) {
+				CompareTest.findOne({ user: req.user }).then((data) => {
+					if (data != null) {
+						compare = new Compare(data.compare);
+						compare.addBeerCompare(response.data.response.beer, beerId);
 
-					CompareTest.findOneAndUpdate({ user: req.user }, { $set: { compare: compare } }, function (err) {
-						if (err) {
-							req.flash('error', err.message);
-							return res.redirect('/compare/my-comparison');
-						}
-					});
-				} else {
-					compare = new Compare({});
-					compare.addBeerCompare(response.data.response.beer, beerId);
+						CompareTest.findOneAndUpdate({ user: req.user }, { $set: { compare: compare } }, function (err) {
+							if (err) {
+								req.flash('error', err.message);
+								return res.redirect('/compare/my-comparison');
+							}
+						});
+					} else {
+						compare = new Compare({});
+						compare.addBeerCompare(response.data.response.beer, beerId);
 
-					let compareTest = new CompareTest({
-						user: req.user,
-						compare: compare
-					});
+						let compareTest = new CompareTest({
+							user: req.user,
+							compare: compare
+						});
 
-					compareTest.save(function (err) {
-						if (err) {
-							req.flash('error', err.message);
-							return res.redirect('/compare/my-comparison');
-						}
-					});
-				}
-			});
+						compareTest.save(function (err) {
+							if (err) {
+								req.flash('error', err.message);
+								return res.redirect('/compare/my-comparison');
+							}
+						});
+					}
+				});
+			} else {
+				compare = new Compare(req.session.compare ? req.session.compare : {});
+				compare.addBeerCompare(response.data.response.beer, beerId);
+				req.session.compare = compare;
+			}
 
-			res.redirect('back');
-		})	
-		.catch((error) => console.error(error));
-	} else {
-		//1) Check if my Compare property exists
-		//2) If it does, pass my old Compare
-		//3) Otherwise, pass empty object
-		compare = new Compare(req.session.compare ? req.session.compare : {});
-
-		axios
-		.get(apiMethods.getBeerByIdURI(CLIENT_ID, CLIENT_SECRET, beerId))
-		.then((response) => {
-			let {
-				beer_name,
-				beer_label,
-				beer_label_hd,
-				beer_abv,
-				beer_ibu,
-				beer_description,
-				beer_style,
-				created_at,
-				rating_count,
-				rating_score,
-				contact,
-				location
-			} = response.data.response.beer; // beer data
-
-			let { brewery_name, brewery_label, country_name } = response.data.response.beer.brewery;
-
-			let stars = apiMethods.starRatingElement(response.data.response.beer.rating_score);
-
-			compare.addBeerCompare(response.data.response.beer, beerId);
-			req.session.compare = compare;
 			res.redirect('back');
 		})
-		.catch((error) => console.error(error));		
-	}
+		.catch((error) => console.error(error));
 });
 
 router.get('/delete-from-compare/:bid', (req, res) => {
