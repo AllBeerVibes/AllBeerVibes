017b45f55dfbdb387f54d68d79e17eafe8580ea3
MINHO CHOI <minhocalgary@gmail.com>
2020-08-26 09:46:11 -0600

    fixed error.js(reposition onclick function)

--- public/js/error.js
+++ public/js/error.js
@@ -7,9 +7,3 @@ window.onclick = function(e) {
 window.onscroll = function(e) {
     errorM.style.opacity = 0;
 }
-
-const topButton = document.querySelector('.toTop');
-
-topButton.addEventListener('click', e => {
-    window.scrollTo({top:0,left:0, behavior:'smooth'});
-})
