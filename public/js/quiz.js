a3d759f7eefe9faebe43c9be0d927fad69d0054c
SooeunKim <sooeuk08@gmail.com>
2020-09-01 17:15:06 -0600

    Beer style explanations for quiz added

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,7 +1,9 @@
 var array = [];
 var charArray = ['a','b','c','d','e','f'];
 var answerArray = ["IPA", "Stout", "Witbier", "Sour Ale", "Lager", "Barleywine"];
+var styleExplanationArray = ["IPAs (short for India pale ales) boast strong hop bitterness with piney and floral flavors. These beers also have high alcohol content.", "Stouts are dark beers that are similar to porters, but with stronger roasted flavors. This style also features mid to high alcohol levels.", "Witbier, white beer, bière blanche, or simply witte is a barley/wheat, top-fermented beer brewed mainly in Belgium and the Netherlands. It gets its name due to suspended yeast and wheat proteins which cause the beer to look hazy, or white, when cold.", "Sour beer is the oldest type of beer in history. Nearly all beer used to be at least somewhat sour before pasteurization and sterilization was entirely understood. Sours today are tart-tasting, and made with wild bacteria and yeasts, whereas more familiar beers are made in sterile environments with specific yeast strains.", "Lagers are the crisp, thirst-quenching yellow beers like Budweiser and the like. Lagers are made with bottom-fermenting yeasts that need the liquid they’re fermenting to be cold and still for a longish time.", "Lively and fruity, sometimes sweet or bittersweet. A brew of this strength and complexity can be a challenge to the palate. Expect anything from an amber to a dark brown color, with aromas ranging from rich fruits to bold hops."];
 var answerStyle = '';
+var answerExplanation = '';
 
 //sessionStorage.setItem('quizArray', array);
 
@@ -51,11 +53,12 @@ quizMenu.addEventListener('click', e => {
             var num = charArray.indexOf(nominate);
 
             answerStyle = answerArray[num];
+            answerExplanation = styleExplanationArray[num];;
 
             resultHead.innerHTML = answerStyle;
                        
             var content = 
-                `<p>blah blah blah 2~3 sentence to explain this style</p>
+                `<p>${answerExplanation}</p>
                 <form method='GET' action="/beer/result">
                     <input type="hidden" name="searchterm" value=${answerStyle}/>
                     <button type="submit">Search Beer</button>
